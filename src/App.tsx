cf3691ce99f35620fcc7d21373aea4c96f91854a
geno <90390749+gitdvdhub@users.noreply.github.com>
2023-09-14 15:48:08 -0300

    Add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { Button } from "./components/ui/button"
+import { Separator } from "./components/ui/separator"
+import { Github } from "lucide-react"
 
 function App() {
 
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="min-h-screen flex flex-col">
+      <header className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com 💜 no NLW da Rocketseat
+          </span>
+
+          <Separator orientation="vertical" className="h-6" />
+
+          <Button variant="outline" onClick={() => {window.location.href = 'https://github.com/rocketseat'}}>
+            <Github className="w-4 h-4 mr-2" />
+            GitHub
+          </Button>
+        </div>
+      </header>
     </div>
   )
 }
